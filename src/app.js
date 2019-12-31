776d7f047c725ccf98f74dff3c411d019427de1c
felipeverbanek <felipeverbanek@gmail.com>
2019-12-30 23:27:01 -0300

    Integracao com sentry

--- src/app.js
+++ src/app.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+require('express-async-errors')
+const Sentry = require('@sentry/node')
+const Youch = require('youch')
 require('./database/index')
+
+const routes = require('./routes');
+const sentryConfig = require('./configs/sentry')
 
 class App{
     constructor(){
         this.server = express();
+
+        Sentry.init(sentryConfig)
+
+
         this.moddlewares();
         this.routes();
+        this.exceptionHandler()
     }    
 
     moddlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')))
     }
 
     routes(){
         this.server.use(routes)
+        this.server.use(Sentry.Handlers.errorHandler());
     }
+
+    exceptionHandler(){
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(errors)
+        })
+    }
+
 }
 
 module.exports = new App().server
